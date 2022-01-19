0d7afb81664768824e9b0bb904d456a52e6bda25
qx-leizige <qx_leizige@163.com>
2022-01-19 14:31:56 +0800

    first Spring Cloud Gateway application!

---
+++ gateway-simple-explain/src/main/java/qx/leizige/gateway/Application.java
@@ -0,0 +1,16 @@
+package qx.leizige.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leizige
+ * 2021/01/19
+ * learn:https://spring.io/guides/gs/gateway/
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ gateway-simple-explain/src/main/java/qx/leizige/gateway/config/RouteConfig.java
@@ -0,0 +1,22 @@
+package qx.leizige.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World!"))
+                        .uri("http://httpbin.org:80")
+                )
+                .build();
+    }
+
+}
